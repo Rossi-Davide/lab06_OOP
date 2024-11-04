b7513cfa59f52a06b2828f87f5142c9d2bad8fdd
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-04 16:05:25 +0100

    Cleared unnecessary imports

--- 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
+++ 64-design-with-maps/src/main/java/it/unibo/collections/social/impl/SocialNetworkUserImpl.java
@@ -8,7 +8,6 @@ import it.unibo.collections.social.api.User;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -1,7 +1,6 @@
 package it.unibo.generics.graph.impl;
 
-import java.util.Collection;
-import java.util.Collections;
+
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
