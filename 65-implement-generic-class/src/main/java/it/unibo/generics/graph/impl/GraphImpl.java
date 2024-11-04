b1d138a56bdf4f271c19a32f56f392d8515dd313
Davide Rossi <davide.rossi47@studio.unibo.it>
2024-11-04 16:47:25 +0100

    Implemented getPath in GraphImpl

--- 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
+++ 65-implement-generic-class/src/main/java/it/unibo/generics/graph/impl/GraphImpl.java
@@ -1,10 +1,13 @@
 package it.unibo.generics.graph.impl;
 
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 import it.unibo.generics.graph.api.Graph;
@@ -20,6 +23,39 @@ public class GraphImpl<N> implements Graph<N>{
 
     private boolean nodeExists(final N node){
         return graph.containsKey(node);
+    }
+
+    private List<N> findPathBFS(final N source, final N target){
+        final Map<N,N> visitedNodesAndParent = new HashMap<>();
+        final Queue<N> nodesToVisit = new LinkedList<>();
+
+        visitedNodesAndParent.put(source, null);
+        nodesToVisit.add(source);
+
+        while(!nodesToVisit.isEmpty() && !visitedNodesAndParent.containsKey(target)){
+            N current = nodesToVisit.remove();
+
+            for(N node : linkedNodes(current)){
+                if(!visitedNodesAndParent.containsKey(node)){
+                    visitedNodesAndParent.put(node, current);
+                    nodesToVisit.add(node);
+                }
+            }
+        }
+
+        if(!visitedNodesAndParent.containsKey(target)){
+            return Collections.emptyList();
+        }
+
+        N parent = visitedNodesAndParent.get(target);
+        final List<N> path = new LinkedList<>();
+        path.addFirst(target);
+        while (parent != null) {
+            path.addFirst(parent);
+            parent = visitedNodesAndParent.get(parent);
+        }
+
+        return path;
     }
 
     @Override
@@ -42,8 +78,11 @@ public class GraphImpl<N> implements Graph<N>{
 
     @Override
     public List<N> getPath(N source, N target) {
-        // TODO Auto-generated method stub
-        return null;
+        if(!nodeExists(source) || !nodeExists(target)){
+            return null;
+        }
+
+        return findPathBFS(source, target);
     }
 
     @Override
